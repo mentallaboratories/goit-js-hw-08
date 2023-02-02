ceed98517213b12164b8a67a6859433e130ea36d
mentallaboratories <mentallaboratories@gmail.com>
2023-02-02 22:40:21 +0200

    vimeo done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,15 +1,46 @@
+import throttle from 'lodash.throttle'
 
-import Vimeo from '@vimeo/player';
+import Vimeo from '@vimeo/player'
+
+
 
 const iframe = document.querySelector('iframe');
 
 const player = new Vimeo(iframe);
 
-player.on('play', function() {
-});
+let savedCurrentTime = 0;
+
+const onPlay = function(data) {
+    savedCurrentTime = data.seconds;
+
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(savedCurrentTime));
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', throttle(onPlay,1000));
+  
+
+
+
+
 
 player.getVideoTitle().then(function(title) {
     console.log('title:', title);
     console.log('on')
 });
  
+const time = JSON.parse(localStorage.getItem('videoplayer-current-time'))
+
+player.setCurrentTime(time).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
